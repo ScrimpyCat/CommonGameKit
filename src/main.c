636d8bb73807a90a96f3d90522cea657e9fce78e
Stefan Johnson <ScrimpyCat@gmail.com>
2015-01-03 03:43:38 +1100

    Set up basic window and render thread

--- src/main.c
+++ src/main.c
@@ -7,8 +7,71 @@
 //
 
 #include <stdio.h>
+#include <CommonC/Common.h>
+#include <GLFW/glfw3.h>
+#include <stdatomic.h>
+#include <tinycthread.h>
+
+static void ErrorCallback(int Error, const char *Description)
+{
+    CC_LOG_ERROR("GLFW Error [%d]: %s", Error, Description);
+}
+
+static int RenderLoop(GLFWwindow *Window)
+{
+    glfwMakeContextCurrent(Window);
+    
+    while (!glfwWindowShouldClose(Window))
+    {
+        glfwSwapBuffers(Window);
+    }
+    
+    return EXIT_SUCCESS;
+}
 
 int main(int argc, const char *argv[])
 {
-    return 0;
+    glfwSetErrorCallback(ErrorCallback);
+    
+    if (!glfwInit())
+    {
+        CC_LOG_ERROR("Failed to initalized GLFW");
+        return EXIT_FAILURE;
+    }
+    
+    
+    GLFWwindow *Window = glfwCreateWindow(640, 480, "Blob Game", NULL, NULL);
+    if (!Window)
+    {
+        CC_LOG_ERROR("Failed to create window");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+    
+    
+    int err;
+    thrd_t RenderThread;
+    if ((err = thrd_create(&RenderThread, (thrd_start_t)RenderLoop, Window)) != thrd_success)
+    {
+        //Possibly fallback to single threaded implementation?
+        CC_LOG_ERROR("Failed to create render thread (%d)", err);
+        
+        glfwDestroyWindow(Window);
+        glfwTerminate();
+        
+        return EXIT_FAILURE;
+    }
+    
+    while (!glfwWindowShouldClose(Window))
+    {
+        glfwWaitEvents();
+    }
+    
+    
+    thrd_detach(RenderThread);
+    
+    glfwDestroyWindow(Window);
+    glfwTerminate();
+    
+    return EXIT_SUCCESS;
 }
