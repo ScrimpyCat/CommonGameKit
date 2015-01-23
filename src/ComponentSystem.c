ed58931539e9b3dd6cbe4d49f0ce540abc51edc1
Stefan Johnson <ScrimpyCat@gmail.com>
2015-01-23 21:28:25 +1100

    Implemented system

--- src/ComponentSystem.c
+++ src/ComponentSystem.c
@@ -7,3 +7,265 @@
 //
 
 #include "ComponentSystem.h"
+#include <CommonC/Common.h>
+#include <stdatomic.h>
+
+
+typedef struct {
+    CCComponentSystemID id;
+    CCComponentSystemExecutionType executionType;
+    
+    CCComponentSystemUpdateCallback update;
+    
+    CCComponentSystemHandlesComponentCallback handlesComponent;
+    CCComponentSystemAddingComponentCallback addingComponent;
+    CCComponentSystemRemovingComponentCallback removingComponent;
+    
+    CCComponentSystemTryLockCallback tryLock;
+    CCComponentSystemLockCallback lock;
+    CCComponentSystemUnlockCallback unlock;
+    
+    struct {
+        CCComponentList active, added, removed, destroy;
+        atomic_flag addedLock, removedLock;
+    } components;
+} CCComponentSystem;
+
+static CCComponentSystem *Systems[CCComponentSystemExecutionMax];
+static size_t SystemsCount[CCComponentSystemExecutionMax];
+
+void CCComponentSystemRegister(CCComponentSystemID id, CCComponentSystemExecutionType ExecutionType, CCComponentSystemUpdateCallback Update, CCComponentSystemHandlesComponentCallback HandlesComponent, CCComponentSystemAddingComponentCallback AddingComponent, CCComponentSystemRemovingComponentCallback RemovingComponent, CCComponentSystemTryLockCallback SystemTryLock, CCComponentSystemLockCallback SystemLock, CCComponentSystemUnlockCallback SystemUnlock)
+{
+    CC_SAFE_Realloc(Systems[ExecutionType], sizeof(CCComponentSystem) * ++SystemsCount[ExecutionType],
+                    CC_LOG_ERROR("Failed to add new system (%" PRIu32 ") due to realloc failing. Allocation size: %zu", id, sizeof(CCComponentSystem) * SystemsCount[ExecutionType]);
+                    SystemsCount[ExecutionType]--;
+                    return;
+                    );
+    
+    const size_t Index = SystemsCount[ExecutionType];
+    Systems[ExecutionType][Index] = (CCComponentSystem){
+        .id = id,
+        .executionType = ExecutionType,
+        .update = Update,
+        .handlesComponent = HandlesComponent,
+        .addingComponent = AddingComponent,
+        .removingComponent = RemovingComponent,
+        .tryLock = SystemTryLock,
+        .lock = SystemLock,
+        .unlock = SystemUnlock,
+        .components = {
+            .active = NULL,
+            .added = NULL,
+            .removed = NULL,
+            .destroy = NULL,
+            .addedLock = ATOMIC_FLAG_INIT,
+            .removedLock = ATOMIC_FLAG_INIT
+        }
+    };
+}
+
+void CCComponentSystemDeregister(CCComponentSystemID id, CCComponentSystemExecutionType ExecutionType)
+{
+    //Note: Really only needed for unit tests, so doesn't matter if not correct
+    const size_t Count = SystemsCount[ExecutionType];
+    for (size_t Loop = 0; Loop < Count; Loop++)
+    {
+        if (Systems[ExecutionType][Loop].id == id)
+        {
+            CCLinkedListDestroy((CCLinkedList)Systems[ExecutionType][Loop].components.active);
+            CCLinkedListDestroy((CCLinkedList)Systems[ExecutionType][Loop].components.added);
+            CCLinkedListDestroy((CCLinkedList)Systems[ExecutionType][Loop].components.removed);
+            CCLinkedListDestroy((CCLinkedList)Systems[ExecutionType][Loop].components.destroy);
+            
+            memset(&Systems[ExecutionType][Loop], 0, sizeof(CCComponentSystem));
+            break;
+        }
+    }
+}
+
+void CCComponentSystemRun(CCComponentSystemExecutionType ExecutionType)
+{
+    const size_t Count = SystemsCount[ExecutionType];
+    for (size_t Loop = 0; Loop < Count; Loop++)
+    {
+        if (Systems[ExecutionType][Loop].update)
+        {
+            if (Systems[ExecutionType][Loop].lock) Systems[ExecutionType][Loop].lock();
+            Systems[ExecutionType][Loop].update(NULL, Systems[ExecutionType][Loop].components.active);
+            if (Systems[ExecutionType][Loop].unlock) Systems[ExecutionType][Loop].unlock();
+        }
+    }
+}
+
+static CCComponentSystem *CCComponentSystemHandlesComponentFind(CCComponent Component)
+{
+    for (size_t Loop = 0; Loop < CCComponentSystemExecutionMax; Loop++)
+    {
+        const size_t Count = SystemsCount[Loop];
+        for (size_t Loop2 = 0; Loop2 < Count; Loop2++)
+        {
+            CCComponentSystem *System = &Systems[Loop][Loop2];
+            if ((System->handlesComponent) && (System->handlesComponent(CCComponentGetID(Component)))) return System;
+        }
+    }
+    
+    return NULL;
+}
+
+void CCComponentSystemAddComponent(CCComponent Component)
+{
+    CCComponentSystem *System = CCComponentSystemHandlesComponentFind(Component);
+    if (System)
+    {
+        CCLinkedListNode *Node = CCLinkedListCreateNode(CC_STD_ALLOCATOR, sizeof(CCComponent), &Component);
+        
+        if (System->addingComponent)
+        {
+            System->addingComponent(Component);
+            
+            if (System->lock) System->lock();
+            System->components.active = System->components.active ? (CCComponentList)CCLinkedListInsert((CCLinkedList)System->components.active , Node) : (CCComponentList)Node;
+            if (System->unlock) System->unlock();
+        }
+        
+        else
+        {
+            while (!atomic_flag_test_and_set(&System->components.addedLock));
+            System->components.added = System->components.added ? (CCComponentList)CCLinkedListInsert((CCLinkedList)System->components.added, Node) : (CCComponentList)Node;
+            atomic_flag_clear(&System->components.addedLock);
+        }
+    }
+}
+
+void CCComponentSystemRemoveComponent(CCComponent Component)
+{
+    CCComponentSystem *System = CCComponentSystemHandlesComponentFind(Component);
+    if (System)
+    {
+        if (System->removingComponent)
+        {
+            System->removingComponent(Component);
+            
+            if (System->lock) System->lock();
+            
+            CCComponentList List = System->components.active;
+            do
+            {
+                if (List->component == Component) break;
+            } while ((List = CCComponentListEnumerateNext(List)));
+            
+            if (List) CCLinkedListDestroyNode((CCLinkedListNode*)List);
+            
+            if (System->unlock) System->unlock();
+        }
+        
+        else
+        {
+            CCLinkedListNode *Node = CCLinkedListCreateNode(CC_STD_ALLOCATOR, sizeof(CCComponent), &Component);
+            
+            while (!atomic_flag_test_and_set(&System->components.removedLock));
+            System->components.removed = System->components.removed ? (CCComponentList)CCLinkedListInsert((CCLinkedList)System->components.removed, Node) : (CCComponentList)Node;
+            atomic_flag_clear(&System->components.removedLock);
+        }
+    }
+}
+
+static CCComponentSystem *CCComponentSystemFind(CCComponentSystemID id)
+{
+    for (size_t Loop = 0; Loop < CCComponentSystemExecutionMax; Loop++)
+    {
+        const size_t Count = SystemsCount[Loop];
+        for (size_t Loop2 = 0; Loop2 < Count; Loop2++)
+        {
+            if (Systems[Loop][Loop2].id == id) return &Systems[Loop][Loop2];
+        }
+    }
+    
+    return NULL;
+}
+
+CCComponentList CCComponentSystemGetComponentsForSystem(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    return System ? System->components.active : NULL;
+}
+
+CCComponentList CCComponentSystemGetAddedComponentsForSystem(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    
+    CCComponentList List = NULL;
+    if ((System) && (System->components.added))
+    {
+        List = System->components.added;
+        
+        while (!atomic_flag_test_and_set(&System->components.addedLock));
+        System->components.active = System->components.active ? (CCComponentList)CCLinkedListInsert((CCLinkedList)System->components.active, (CCLinkedList)System->components.added) : System->components.added;
+        
+        System->components.added = NULL;
+        atomic_flag_clear(&System->components.addedLock);
+    }
+    
+    return List;
+}
+
+CCComponentList CCComponentSystemGetRemovedComponentsForSystem(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    
+    CCComponentList List = NULL;
+    if ((System) && (System->components.removed))
+    {
+        if (System->components.destroy)
+        {
+            CCLinkedListDestroy((CCLinkedList)System->components.destroy);
+            System->components.destroy = NULL;
+        }
+        
+        List = System->components.removed;
+        
+        if (System->components.active)
+        {
+            CCComponentList Active = System->components.active, Removed = System->components.removed;
+            do
+            {
+                if (CCComponentListGetCurrentComponent(Active) == CCComponentListGetCurrentComponent(Removed))
+                {
+                    Removed = CCComponentListEnumerateNext(Removed);
+                    
+                    CCComponentList Dead = Active;
+                    Active = CCComponentListEnumerateNext(Active);
+                    CCLinkedListDestroyNode((CCLinkedList)Dead);
+                    continue;
+                }
+            } while ((Active = CCComponentListEnumerateNext(Active)));
+        }
+        
+        System->components.destroy = System->components.removed;
+        System->components.removed = NULL;
+    }
+    
+    return List;
+}
+
+_Bool CCComponentSystemTryLock(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    
+    _Bool Locked = TRUE;
+    if ((System) && (System->tryLock)) Locked = System->tryLock();
+    
+    return Locked;
+}
+
+void CCComponentSystemLock(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    if ((System) && (System->lock)) System->lock();
+}
+
+void CCComponentSystemUnlock(CCComponentSystemID id)
+{
+    CCComponentSystem *System = CCComponentSystemFind(id);
+    if ((System) && (System->unlock)) System->unlock();
+}
--- src/ComponentSystem.h
+++ src/ComponentSystem.h
@@ -9,5 +9,205 @@
 #ifndef Blob_Game_ComponentSystem_h
 #define Blob_Game_ComponentSystem_h
 
+#include <stdint.h>
+#include "Component.h"
+
+/*!
+ * @typedef CCComponentSystemExecutionType
+ * @brief The execution type of the system.
+ * @constant CCComponentSystemExecutionTypeManual Needs to be manually called (CCComponentSystemUpdateCallback).
+ * @constant CCComponentSystemExecutionTypeRender Runs on the render thread (CCComponentSystemTimedUpdateCallback).
+ * @constant CCComponentSystemExecutionTypePhysics Runs on the physics thread (CCComponentSystemTimedUpdateCallback).
+ * @constant CCComponentSystemExecutionTypeUpdate Runs on the update thread (CCComponentSystemTimedUpdateCallback).
+ * @constant CCComponentSystemExecutionTypeInput Runs on the input/main thread (CCComponentSystemUpdateCallback).
+ * @constant CCComponentSystemExecutionTypeIO Runs on the IO thread (CCComponentSystemUpdateCallback).
+ * @constant CCComponentSystemExecutionMax The maximum amount of execution types.
+ */
+typedef enum {
+    ///Needs to be manually called (CCComponentSystemUpdateCallback).
+    CCComponentSystemExecutionTypeManual,
+    ///Runs on the render thread (CCComponentSystemTimedUpdateCallback).
+    CCComponentSystemExecutionTypeRender,
+    ///Runs on the physics thread (CCComponentSystemTimedUpdateCallback).
+    CCComponentSystemExecutionTypePhysics,
+    ///Runs on the update thread (CCComponentSystemTimedUpdateCallback).
+    CCComponentSystemExecutionTypeUpdate,
+    ///Runs on the input/main thread (CCComponentSystemUpdateCallback).
+    CCComponentSystemExecutionTypeInput,
+    ///Runs on the IO thread (CCComponentSystemUpdateCallback).
+    CCComponentSystemExecutionTypeIO,
+    ///The maximum amount of execution types.
+    CCComponentSystemExecutionMax
+} CCComponentSystemExecutionType;
+
+/*!
+ * @brief The unique ID of the system.
+ */
+typedef uint32_t CCComponentSystemID;
+
+typedef struct {
+    CCLinkedListNode node;
+    CCComponent component;
+} *CCComponentList;
+
+
+/*!
+ * @brief Generic system update callback.
+ */
+typedef void (*CCComponentSystemUpdateCallback)(void *Context, CCComponentList Components);
+
+/*!
+ * @brief Time based system update callback.
+ */
+typedef void (*CCComponentSystemTimedUpdateCallback)(double DeltaTime, CCComponentList Components);
+
+
+/*!
+ * @brief Handles component callback.
+ * @description Determine whether a system has ownership (manages) of a particular component type.
+ */
+typedef _Bool (*CCComponentSystemHandlesComponentCallback)(CCComponentID id);
+
+/*!
+ * @brief Add component callback.
+ * @description Called when a new component has been added to the system. If this is not implemented
+ *              an explicit call to CCComponentSystemGetAddedComponentsForSystem must be made.
+ *
+ * @performance This callback may be less efficient than CCComponentSystemGetAddedComponentsForSystem
+ *              as this incurs a system lock each time a component is added.
+ */
+typedef void (*CCComponentSystemAddingComponentCallback)(CCComponent Component);
+
+/*!
+ * @brief Remove component callback.
+ * @description Called when a component has been removed from the system. If this is not implemented
+ *              an explicit call to CCComponentSystemGetRemovedComponentsForSystem must be made.
+ */
+typedef void (*CCComponentSystemRemovingComponentCallback)(CCComponent Component);
+
+
+/*!
+ * @brief Try lock system callback.
+ * @description Attempt to lock the system. It should be a recursive lock.
+ */
+typedef _Bool (*CCComponentSystemTryLockCallback)(void);
+
+/*!
+ * @brief Lock system callback.
+ * @description Lock the system. It should be a recursive lock.
+ */
+typedef void (*CCComponentSystemLockCallback)(void);
+
+/*!
+ * @brief Unlock system callback.
+ * @description Unlock the system. It should be a recursive lock.
+ */
+typedef void (*CCComponentSystemUnlockCallback)(void);
+
+
+/*!
+ * @brief Register a system.
+ * @param id The unique ID of the system.
+ * @param ExecutionType How the system should be executed.
+ * @param Update The update callback of the system. Type of update depends on execution type. May be NULL.
+ * @param HandlesComponent The callback to determine if the system manages the component. May be NULL.
+ * @param AddingComponent The callback to let the system know a component has been added. May be NULL,
+ *        must then call CCComponentSystemGetAddedComponentsForSystem.
+ *
+ * @param RemovingComponent The callback to let the system know a component has been removed. May be NULL,
+ *        must then call CCComponentSystemGetRemovedComponentsForSystem.
+ *
+ * @param TryLock The callback to attempt a try lock. May be NULL.
+ * @param Lock The callback to lock the system. May be NULL.
+ * @param Unlock The callback to unlock the system. May be NULL.
+ */
+void CCComponentSystemRegister(CCComponentSystemID id, CCComponentSystemExecutionType ExecutionType, CCComponentSystemUpdateCallback Update, CCComponentSystemHandlesComponentCallback HandlesComponent, CCComponentSystemAddingComponentCallback AddingComponent, CCComponentSystemRemovingComponentCallback RemovingComponent, CCComponentSystemTryLockCallback TryLock, CCComponentSystemLockCallback Lock, CCComponentSystemUnlockCallback Unlock);
+
+/*!
+ * @brief Deregister the system.
+ * @warning Only provided for unit tests, not safe to call in actual program.
+ * @param id The unique ID of the system.
+ * @param ExecutionType The execution type the system should be removed from.
+ */
+void CCComponentSystemDeregister(CCComponentSystemID id, CCComponentSystemExecutionType ExecutionType);
+
+/*!
+ * @brief Run the systems of a particular execution type.
+ * @description Locks the systems and calls their update callbacks.
+ * @param ExecutionType The execution type to be executed.
+ */
+void CCComponentSystemRun(CCComponentSystemExecutionType ExecutionType);
+
+/*!
+ * @brief Get the current components for the system.
+ * @warning Should obtain locks to the system prior to calling this function.
+ * @return The current component list.
+ */
+CCComponentList CCComponentSystemGetComponentsForSystem(CCComponentSystemID id);
+
+/*!
+ * @brief Get the current added components for the system.
+ * @description Adds those components to the main component list of the system, and clears the added components
+ *              list.
+ *
+ * @warning This call must be made within a system lock.
+ *
+ * @param id The unique ID of the system.
+ * @return The component list of the added components.
+ */
+CCComponentList CCComponentSystemGetAddedComponentsForSystem(CCComponentSystemID id);
+
+/*!
+ * @brief Get the current removed components for the system.
+ * @description Removes those components from the main component list of the system, and clears the removed components
+ *              list.
+ *
+ * @warning This call must be made within a system lock.
+ *
+ * @param id The unique ID of the system.
+ * @return The component list of the removed components.
+ */
+CCComponentList CCComponentSystemGetRemovedComponentsForSystem(CCComponentSystemID id);
+
+/*!
+ * @brief Attempt to lock the system.
+ * @param The unique ID of the system.
+ * @return True if was able to lock the system, otherwise false.
+ */
+_Bool CCComponentSystemTryLock(CCComponentSystemID id);
+
+/*!
+ * @brief Lock the system.
+ * @param The unique ID of the system.
+ */
+void CCComponentSystemLock(CCComponentSystemID id);
+
+/*!
+ * @brief Unlock the system.
+ * @param The unique ID of the system.
+ */
+void CCComponentSystemUnlock(CCComponentSystemID id);
+
+
+#pragma mark - Component List Functions
+/*!
+ * @brief Enumerate to the next component in the list.
+ * @param List The current list.
+ * @return The new list where the current component is the next one.
+ */
+static inline CCComponentList CCComponentListEnumerateNext(CCComponentList List)
+{
+    return (CCComponentList)CCLinkedListEnumerateNext((CCLinkedList)List);
+}
+
+/*!
+ * @brief Get the current component from the list.
+ * @param List The current list.
+ * @return The current component in the list.
+ */
+static inline CCComponent CCComponentListGetCurrentComponent(CCComponentList List)
+{
+    return List? List->component : NULL;
+}
 
 #endif
