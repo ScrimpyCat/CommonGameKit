5c1421865e315635d42ca059dbea005f80c589a1
Stefan Johnson <ScrimpyCat@gmail.com>
2015-01-24 23:16:58 +1100

    Added tests for component adding/removal

--- Blob GameTests/ComponentSystem.m
+++ Blob GameTests/ComponentSystem.m
@@ -57,16 +57,79 @@ static void TestSystemAddingComponentNonCallbackUpdate(void *Context, CCComponen
     TestSystemAddingComponentNonCallbackHasActiveComponents = *Components != NULL;
 }
 
+static _Bool TestSystemAddingComponentNonCallbackHandlesComponent(CCComponentID id)
+{
+    return id == TEST_SYSTEM_ID;
+}
+
 -(void) testSystemAddingComponentNonCallback
 {
-    CCComponentSystemRegister(TEST_SYSTEM_ID, CCComponentSystemExecutionTypeManual, TestSystemAddingComponentNonCallbackUpdate, NULL, NULL, NULL, NULL, NULL, NULL);
+    CCComponentSystemRegister(TEST_SYSTEM_ID, CCComponentSystemExecutionTypeManual, TestSystemAddingComponentNonCallbackUpdate, TestSystemAddingComponentNonCallbackHandlesComponent, NULL, NULL, NULL, NULL, NULL);
     
     CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
     XCTAssertFalse(TestSystemAddingComponentNonCallbackHasActiveComponents, @"Should not have any active components");
     XCTAssertFalse(TestSystemAddingComponentNonCallbackHasAddedComponents, @"Should not have any added components");
     XCTAssertFalse(TestSystemAddingComponentNonCallbackHasRemovedComponents, @"Should not have any removed components");
 
+    CCComponent Component = CCComponentCreate(CC_COMPONENT_ID);
+    CCComponentSystemAddComponent(Component);
     
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertTrue(TestSystemAddingComponentNonCallbackHasActiveComponents, @"Should have active components"); //as added gets added to active
+    XCTAssertTrue(TestSystemAddingComponentNonCallbackHasAddedComponents, @"Should have added components");
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasRemovedComponents, @"Should not have any removed components");
+    
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertTrue(TestSystemAddingComponentNonCallbackHasActiveComponents, @"Should have active components");
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasAddedComponents, @"Should not have any added components"); //as added has been cleared
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasRemovedComponents, @"Should not have any removed components");
+    
+    CCComponentSystemRemoveComponent(Component);
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasActiveComponents, @"Should not have any active components"); //gets removed
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasAddedComponents, @"Should not have any added components");
+    XCTAssertTrue(TestSystemAddingComponentNonCallbackHasRemovedComponents, @"Should have removed components");
+    
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasActiveComponents, @"Should not have any active components");
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasAddedComponents, @"Should not have any added components");
+    XCTAssertFalse(TestSystemAddingComponentNonCallbackHasRemovedComponents, @"Should not have any removed components"); //as removed has been cleared
+    
+    CCComponentSystemDeregister(TEST_SYSTEM_ID, CCComponentSystemExecutionTypeManual);
+}
+
+static _Bool TestSystemAddingComponentCallbackHasActiveComponents;
+static void TestSystemAddingComponentCallbackUpdate(void *Context, CCComponentList *Components)
+{
+    TestSystemAddingComponentCallbackHasActiveComponents = *Components != NULL;
+}
+
+static void TestSystemAddingComponentCallbackAddingComponent(CCComponent Component)
+{
+    
+}
+
+static void TestSystemAddingComponentCallbackRemovingComponent(CCComponent Component)
+{
+    
+}
+
+-(void) testSystemAddingComponentCallback
+{
+    CCComponentSystemRegister(TEST_SYSTEM_ID, CCComponentSystemExecutionTypeManual, TestSystemAddingComponentCallbackUpdate, TestSystemAddingComponentNonCallbackHandlesComponent, TestSystemAddingComponentCallbackAddingComponent, TestSystemAddingComponentCallbackRemovingComponent, NULL, NULL, NULL);
+    
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertFalse(TestSystemAddingComponentCallbackHasActiveComponents, @"Should not have any active components");
+    
+    CCComponent Component = CCComponentCreate(CC_COMPONENT_ID);
+    CCComponentSystemAddComponent(Component);
+    
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertTrue(TestSystemAddingComponentCallbackHasActiveComponents, @"Should have active components");
+        
+    CCComponentSystemRemoveComponent(Component);
+    CCComponentSystemRun(CCComponentSystemExecutionTypeManual);
+    XCTAssertFalse(TestSystemAddingComponentCallbackHasActiveComponents, @"Should not have any active components");
     
     CCComponentSystemDeregister(TEST_SYSTEM_ID, CCComponentSystemExecutionTypeManual);
 }
