6a107d5a0ef594d3bbccc2e9edfbf48f2581a725
Stefan Johnson <sj@scrimpycat.io>
2016-05-01 11:35:14 +1000

    Use vertex colour in signed distance fields

--- assets/shaders/signed-distance-field.fs
+++ assets/shaders/signed-distance-field.fs
@@ -1,10 +1,10 @@
 #version 330 core
 
 uniform sampler2D tex;
-uniform vec4 colour;
 uniform float width;
 uniform float edge;
 
+in vec4 colour;
 in vec2 texCoord;
 
 out vec4 fragColour;
@@ -14,5 +14,5 @@ void main()
     float dist = 1.0 - texture(tex, texCoord).a;
     float a = 1.0 - smoothstep(width, width + edge, dist);
 
-    fragColour = vec4(colour.rgb, a);
+    fragColour = vec4(colour.rgb, colour.a * a);
 }
