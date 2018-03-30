15c4e272d084ffca6a6fcc0d4f04160d52011b64
Stefan Johnson <sj@scrimpycat.io>
2018-03-30 21:58:19 +1100

    Set deployment target of zlib and libpng

--- mac-setup.sh
+++ mac-setup.sh
@@ -2,9 +2,12 @@ cd deps/zlib
 rm -rf build
 mkdir build && cd build
 cmake -G Xcode ..
+sed -i -e 's/SDKROOT/MACOSX_DEPLOYMENT_TARGET = 10.10; SDKROOT/' zlib.xcodeproj/project.pbxproj
+
 cd ../../libpng
 cp scripts/pnglibconf.h.prebuilt pnglibconf.h
 rm -rf build
 mkdir build && cd build
 cmake -DPNG_BUILD_ZLIB=ON -DZLIB_INCLUDE_DIR=../zlib -G Xcode ..
 sed -i -e 's/"make.*postBuildPhase.*"/""/' libpng.xcodeproj/project.pbxproj
+sed -i -e 's/SDKROOT/MACOSX_DEPLOYMENT_TARGET = 10.10; SDKROOT/' libpng.xcodeproj/project.pbxproj
