8e9f1c94b1cef22cc777347c6ff24881dc947120
Stefan Johnson <sj@scrimpycat.io>
2018-09-08 21:44:49 +1000

    Test custom data

--- CommonGameKitTests/ExpressionComponentTests.m
+++ CommonGameKitTests/ExpressionComponentTests.m
@@ -64,6 +64,7 @@ static void SetBool(CCComponent Component, _Bool Value){ snprintf(Set, sizeof(Se
 static void SetF32(CCComponent Component, float Value){ snprintf(Set, sizeof(Set), "%.2f", Value); }
 static void SetF64(CCComponent Component, double Value){ snprintf(Set, sizeof(Set), "%.2f", Value); }
 static void SetColour(CCComponent Component, CCColourRGBA Value){ snprintf(Set, sizeof(Set), "%.2f, %.2f, %.2f, %.2f", Value.r, Value.g, Value.b, Value.a); }
+static void SetData(CCComponent Component, int *Value){ snprintf(Set, sizeof(Set), "->%d", *Value); CCFree(Value); }
 
 static CCComponentExpressionArgumentDeserializer Arguments[] = {
     { .name = CC_STRING("i8:"), .serializedType = CCExpressionValueTypeUnspecified, .setterType = CCComponentExpressionArgumentTypeInt8, .setter = (CCComponentExpressionSetter)SetI8 },
@@ -79,7 +80,26 @@ static CCComponentExpressionArgumentDeserializer Arguments[] = {
     { .name = CC_STRING("f32:"), .serializedType = CCExpressionValueTypeUnspecified, .setterType = CCComponentExpressionArgumentTypeFloat32, .setter = (CCComponentExpressionSetter)SetF32 },
     { .name = CC_STRING("f64:"), .serializedType = CCExpressionValueTypeUnspecified, .setterType = CCComponentExpressionArgumentTypeFloat64, .setter = (CCComponentExpressionSetter)SetF64 },
     { .name = CC_STRING("colour:"), .serializedType = CCExpressionValueTypeUnspecified, .setterType = CCComponentExpressionArgumentTypeColour, .setter = (CCComponentExpressionSetter)SetColour },
+    { .name = CC_STRING("data:"), .serializedType = 'test', .setterType = CCComponentExpressionArgumentTypeData, .setter = (CCComponentExpressionSetter)SetData }
 };
+
+static size_t DestroyCount = 0;
+static void TestDataDestructor(void *Ptr)
+{
+    DestroyCount++;
+}
+
+static CCExpression TestDataEvaluator(CCExpression Expression)
+{
+    int *Data;
+    CC_SAFE_Malloc(Data, sizeof(int));
+    
+    CCMemorySetDestructor(Data, TestDataDestructor);
+    
+    *Data = CCExpressionGetInteger(CCExpressionEvaluate(*(CCExpression*)CCOrderedCollectionGetElementAtIndex(CCExpressionGetList(Expression), 1)));
+    
+    return CCExpressionCreateCustomType(CC_STD_ALLOCATOR, 'test', Data, NULL, CCFree);
+}
 
 -(void) testDeserialization
 {
@@ -88,6 +108,14 @@ do { \
     Set[0] = 0; \
     CCExpression Arg = CCExpressionCreateFromSource(expression); \
     XCTAssertTrue(CCComponentExpressionDeserializeArgument(Component, Arg, Arguments, sizeof(Arguments) / sizeof(typeof(*Arguments)), FALSE), @"Should deserialize"); \
+    XCTAssertTrue(!strcmp(Set, result), @"Should have the result (%s) got (%s)", result, Set); \
+    CCExpressionDestroy(Arg); \
+} while(0)
+#define TEST_EVALUATE_DESERIALIZE_SUCCESS(expression, result) \
+do { \
+    Set[0] = 0; \
+    CCExpression Arg = CCExpressionCreateFromSource(expression); \
+    XCTAssertTrue(CCComponentExpressionDeserializeArgument(Component, CCExpressionEvaluate(Arg), Arguments, sizeof(Arguments) / sizeof(typeof(*Arguments)), FALSE), @"Should deserialize"); \
     XCTAssertTrue(!strcmp(Set, result), @"Should have the result (%s) got (%s)", result, Set); \
     CCExpressionDestroy(Arg); \
 } while(0)
@@ -107,62 +135,62 @@ do { \
     TEST_DESERIALIZE_SUCCESS("(i8: 127)", "127");
     TEST_DESERIALIZE_SUCCESS("(i8: -128)", "-128");
     TEST_DESERIALIZE_SUCCESS("(i8: 256)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(i16: :d)");
     TEST_DESERIALIZE_SUCCESS("(i16: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(i16: -1)", "-1");
     TEST_DESERIALIZE_SUCCESS("(i16: 32767)", "32767");
     TEST_DESERIALIZE_SUCCESS("(i16: -32768)", "-32768");
     TEST_DESERIALIZE_SUCCESS("(i16: 65536)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(i32: :d)");
     TEST_DESERIALIZE_SUCCESS("(i32: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(i32: -1)", "-1");
     TEST_DESERIALIZE_SUCCESS("(i32: 2147483647)", "2147483647");
     TEST_DESERIALIZE_SUCCESS("(i32: -2147483648)", "-2147483648");
     TEST_DESERIALIZE_SUCCESS("(i32: 4294967296)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(i64: :d)");
     TEST_DESERIALIZE_SUCCESS("(i64: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(i64: -1)", "-1");
     TEST_DESERIALIZE_SUCCESS("(i64: 2147483647)", "2147483647");
     TEST_DESERIALIZE_SUCCESS("(i64: -2147483648)", "-2147483648");
     TEST_DESERIALIZE_SUCCESS("(i64: 4294967296)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(u8: :d)");
     TEST_DESERIALIZE_SUCCESS("(u8: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(u8: -1)", "255");
     TEST_DESERIALIZE_SUCCESS("(u8: 127)", "127");
     TEST_DESERIALIZE_SUCCESS("(u8: -128)", "128");
     TEST_DESERIALIZE_SUCCESS("(u8: 256)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(u16: :d)");
     TEST_DESERIALIZE_SUCCESS("(u16: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(u16: -1)", "65535");
     TEST_DESERIALIZE_SUCCESS("(u16: 32767)", "32767");
     TEST_DESERIALIZE_SUCCESS("(u16: -32768)", "32768");
     TEST_DESERIALIZE_SUCCESS("(u16: 65536)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(u32: :d)");
     TEST_DESERIALIZE_SUCCESS("(u32: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(u32: -1)", "4294967295");
     TEST_DESERIALIZE_SUCCESS("(u32: 2147483647)", "2147483647");
     TEST_DESERIALIZE_SUCCESS("(u32: -2147483648)", "2147483648");
     TEST_DESERIALIZE_SUCCESS("(u32: 4294967296)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(u64: :d)");
     TEST_DESERIALIZE_SUCCESS("(u64: 1)", "1");
     TEST_DESERIALIZE_SUCCESS("(u64: -1)", "18446744073709551615");
     TEST_DESERIALIZE_SUCCESS("(u64: 2147483647)", "2147483647");
     TEST_DESERIALIZE_SUCCESS("(u64: -2147483648)", "18446744071562067968");
     TEST_DESERIALIZE_SUCCESS("(u64: 4294967296)", "0");
-    
+
     TEST_DESERIALIZE_FAILURE("(str: :d)");
     TEST_DESERIALIZE_FAILURE("(str: 1)");
     TEST_DESERIALIZE_SUCCESS("(str: \"foo\")", "foo");
     TEST_DESERIALIZE_SUCCESS("(str: \"bar\")", "bar");
     TEST_DESERIALIZE_SUCCESS("(str: \"\")", "");
-    
+
     TEST_DESERIALIZE_FAILURE("(bool: :d)");
     TEST_DESERIALIZE_SUCCESS("(bool: 1)", "true");
     TEST_DESERIALIZE_SUCCESS("(bool: #f)", "false");
@@ -170,19 +198,19 @@ do { \
     TEST_DESERIALIZE_SUCCESS("(bool: 10)", "true");
     TEST_DESERIALIZE_SUCCESS("(bool: 0)", "false");
     TEST_DESERIALIZE_SUCCESS("(bool: -1)", "true");
-    
+
     TEST_DESERIALIZE_FAILURE("(f32: :d)");
     TEST_DESERIALIZE_FAILURE("(f32: 1)");
     TEST_DESERIALIZE_SUCCESS("(f32: -1.0)", "-1.00");
     TEST_DESERIALIZE_SUCCESS("(f32: 1.5)", "1.50");
     TEST_DESERIALIZE_SUCCESS("(f32: 50.12)", "50.12");
-    
+
     TEST_DESERIALIZE_FAILURE("(f64: :d)");
     TEST_DESERIALIZE_FAILURE("(f64: 1)");
     TEST_DESERIALIZE_SUCCESS("(f64: -1.0)", "-1.00");
     TEST_DESERIALIZE_SUCCESS("(f64: 1.5)", "1.50");
     TEST_DESERIALIZE_SUCCESS("(f64: 50.12)", "50.12");
-    
+
     TEST_DESERIALIZE_FAILURE("(colour: :d)");
     TEST_DESERIALIZE_FAILURE("(colour: 1)");
     TEST_DESERIALIZE_FAILURE("(colour: 1.0)");
@@ -196,6 +224,15 @@ do { \
     TEST_DESERIALIZE_SUCCESS("(colour: (0 64 127))", "0.00, 0.25, 0.50, 1.00");
     TEST_DESERIALIZE_SUCCESS("(colour: (1.0 0.75 0.5 0.25))", "1.00, 0.75, 0.50, 0.25");
     TEST_DESERIALIZE_SUCCESS("(colour: (1.0 0.75 0.5))", "1.00, 0.75, 0.50, 1.00");
+    
+    CCExpressionEvaluatorRegister(CC_STRING("test-data"), TestDataEvaluator);
+    TEST_DESERIALIZE_FAILURE("(data: :d)");
+    TEST_DESERIALIZE_FAILURE("(data: 1)");
+    TEST_DESERIALIZE_FAILURE("(data: 1.0)");
+    TEST_DESERIALIZE_FAILURE("(data: (test-data 1))");
+    TEST_EVALUATE_DESERIALIZE_SUCCESS("(data: (test-data 1))", "->1");
+    TEST_EVALUATE_DESERIALIZE_SUCCESS("(data: (test-data 2))", "->2");
+    XCTAssertEqual(DestroyCount, 2, @"Should have destroyed the data");
     
     CCComponentDestroy(Component);
 }
