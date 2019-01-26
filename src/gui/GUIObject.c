78dc7b0a6d006a1ddf1ff8d68d9635b3fc9faa1e
Stefan Johnson <sj@scrimpycat.io>
2019-01-26 15:07:13 +1100

    Add caching to GUI

--- src/gui/GUIObject.c
+++ src/gui/GUIObject.c
@@ -25,6 +25,7 @@
 
 #include "GUIObject.h"
 #include "Window.h"
+#include "AssetManager.h"
 
 GUIObject GUIObjectCreate(CCAllocatorType Allocator, const GUIObjectInterface *Interface)
 {
@@ -38,7 +39,12 @@ GUIObject GUIObjectCreate(CCAllocatorType Allocator, const GUIObjectInterface *I
             .allocator = Allocator,
             .parent = NULL,
             .internal = Interface->create(Allocator),
-            .changed = TRUE
+            .cache = {
+                .strategy = GUIObjectCacheStrategyNone,
+                .rect = (CCRect){ .position = CCVector2DZero, .size = CCVector2DZero },
+                .store = NULL,
+                .changed = TRUE
+            }
         };
         
         if (!Object->internal)
@@ -68,12 +74,10 @@ void GUIObjectDestroy(GUIObject Object)
         GUIObjectRemoveChild(Parent, Object);
     }
     
-    else
-    {
-        mtx_destroy(&Object->lock);
-        Object->interface->destroy(Object->internal);
-        CC_SAFE_Free(Object);
-    }
+    mtx_destroy(&Object->lock);
+    if (Object->cache.store) GFXFramebufferDestroy(Object->cache.store);
+    Object->interface->destroy(Object->internal);
+    CC_SAFE_Free(Object);
 }
 
 void GUIObjectRender(GUIObject Object, GFXFramebuffer Framebuffer, size_t Index, GFXBuffer Projection)
@@ -81,8 +85,74 @@ void GUIObjectRender(GUIObject Object, GFXFramebuffer Framebuffer, size_t Index,
     CCAssertLog(Object, "GUI object must not be null");
     
     mtx_lock(&Object->lock);
-    if (GUIObjectGetEnabled(Object)) Object->interface->render(Object, Framebuffer, Index, Projection);
-    Object->changed = FALSE;
+    
+    if (GUIObjectGetEnabled(Object))
+    {
+        if ((Object->cache.strategy & GUIObjectCacheStrategyMask) != GUIObjectCacheStrategyNone)
+        {
+            if ((Object->cache.store) && (Object->cache.strategy & GUIObjectCacheDirtyResize))
+            {
+                GFXFramebufferDestroy(Object->cache.store);
+                Object->cache.store = NULL;
+            }
+            
+            GFXTexture Texture;
+            const CCRect Rect = GUIObjectGetRect(Object);
+            if (!Object->cache.store)
+            {
+                //TODO: use frame bounds?
+                //TODO: Use texture streams?
+                Texture = GFXTextureCreate(CC_STD_ALLOCATOR, GFXTextureHintDimension2D | (GFXTextureHintFilterModeNearest << GFXTextureHintFilterMin) | (GFXTextureHintFilterModeNearest << GFXTextureHintFilterMag), CCColourFormatRGB8Unorm, (size_t)Rect.size.x, (size_t)Rect.size.y, 1, NULL);
+                GFXFramebufferAttachment Attachment = GFXFramebufferAttachmentCreateColour(Texture, GFXFramebufferAttachmentActionFlagClearOnce | GFXFramebufferAttachmentActionLoad, GFXFramebufferAttachmentActionStore, CCVector4DFill(0.0f));
+                Object->cache.store = GFXFramebufferCreate(CC_STD_ALLOCATOR, &Attachment, 1);
+            }
+            
+            else Texture = GFXFramebufferGetAttachment(Object->cache.store, 0)->texture;
+            
+            if (Object->cache.strategy & GUIObjectCacheDirtyMask)
+            {
+                CCMatrix4 Ortho = CCMatrix4MakeOrtho(0.0f, Rect.size.x, 0.0f, Rect.size.y, 0.0f, 1.0f);
+                Ortho = CCMatrix4Translate(Ortho, CCVector3DMake(Rect.position.x, Rect.position.y, 0.0f));
+                GFXBuffer CacheProjection = GFXBufferCreate(CC_STD_ALLOCATOR, GFXBufferHintData | GFXBufferHintCPUWriteOnce | GFXBufferHintGPUReadOnce, sizeof(CCMatrix4), &Ortho);
+                Object->interface->render(Object, Object->cache.store, 0, CacheProjection);
+                Object->cache.strategy &= GUIObjectCacheStrategyMask;
+                
+                GFXBufferDestroy(CacheProjection);
+            }
+            
+            GFXDraw Drawer = GFXDrawCreate(CC_STD_ALLOCATOR);
+            
+            GFXShader Shader = CCAssetManagerCreateShader(CC_STRING("texture2d"));
+            GFXDrawSetShader(Drawer, Shader);
+            GFXShaderDestroy(Shader);
+            
+            struct {
+                CCVector2D position;
+                CCVector2D coord;
+            } VertData[4] = {
+                { .position = Rect.position, .coord = CCVector2DMake(0.0f, 0.0f) },
+                { .position = CCVector2DMake(Rect.position.x + Rect.size.x, Rect.position.y), .coord = CCVector2DMake(1.0f, 0.0f) },
+                { .position = CCVector2DMake(Rect.position.x, Rect.position.y + Rect.size.y), .coord = CCVector2DMake(0.0f, 1.0f) },
+                { .position = CCVector2DMake(Rect.position.x + Rect.size.x, Rect.position.y + Rect.size.y), .coord = CCVector2DMake(1.0f, 1.0f) }
+            };
+            
+            GFXBuffer VertBuffer = GFXBufferCreate(CC_STD_ALLOCATOR, GFXBufferHintDataVertex | GFXBufferHintCPUWriteOnce | GFXBufferHintGPUReadOnce, sizeof(VertData), &VertData);
+            GFXDrawSetVertexBuffer(Drawer, CC_STRING("vPosition"), VertBuffer, GFXBufferFormatFloat32x2, sizeof(typeof(*VertData)), offsetof(typeof(*VertData), position));
+            GFXDrawSetVertexBuffer(Drawer, CC_STRING("vTexCoord"), VertBuffer, GFXBufferFormatFloat32x2, sizeof(typeof(*VertData)), offsetof(typeof(*VertData), coord));
+            GFXBufferDestroy(VertBuffer);
+            
+            GFXDrawSetTexture(Drawer, CC_STRING("tex"), Texture);
+            GFXDrawSetBlending(Drawer, GFXBlendTransparent);
+            GFXDrawSetFramebuffer(Drawer, Framebuffer, Index);
+            GFXDrawSetBuffer(Drawer, CC_STRING("modelViewProjectionMatrix"), Projection);
+            GFXDrawSubmit(Drawer, GFXPrimitiveTypeTriangleStrip, 0, 4);
+            
+            GFXDrawDestroy(Drawer);
+        }
+        
+        else Object->interface->render(Object, Framebuffer, Index, Projection);
+    }
+    Object->cache.changed = FALSE;
     mtx_unlock(&Object->lock);
 }
 
@@ -91,7 +161,11 @@ void GUIObjectEvent(GUIObject Object, GUIEvent Event)
     CCAssertLog(Object, "GUI object must not be null");
     
     mtx_lock(&Object->lock);
-    if (GUIObjectGetEnabled(Object)) Object->interface->event(Object, Event);
+    if (GUIObjectGetEnabled(Object))
+    {
+        Object->cache.strategy |= GUIObjectCacheDirtyEvent;
+        Object->interface->event(Object, Event);
+    }
     mtx_unlock(&Object->lock);
 }
 
@@ -112,7 +186,6 @@ void GUIObjectSetRect(GUIObject Object, CCRect Rect)
     
     mtx_lock(&Object->lock);
     Object->interface->rect.set(Object, Rect);
-    Object->changed = TRUE;
     mtx_unlock(&Object->lock);
 }
 
@@ -143,6 +216,33 @@ GUIObject GUIObjectGetParent(GUIObject Object)
     return Object->parent;
 }
 
+GUIObjectCache GUIObjectGetCacheStrategy(GUIObject Object)
+{
+    CCAssertLog(Object, "GUI object must not be null");
+    
+    mtx_lock(&Object->lock);
+    const GUIObjectCache Cache = Object->cache.strategy & GUIObjectCacheStrategyMask;
+    mtx_unlock(&Object->lock);
+    
+    return Cache;
+}
+
+void GUIObjectSetCacheStrategy(GUIObject Object, GUIObjectCache Strategy)
+{
+    CCAssertLog(Object, "GUI object must not be null");
+    
+    mtx_lock(&Object->lock);
+    Object->cache.strategy = Strategy & GUIObjectCacheStrategyMask;
+    Object->cache.changed = TRUE;
+    
+    if ((Object->cache.store) && ((Strategy & GUIObjectCacheStrategyMask) == GUIObjectCacheStrategyNone))
+    {
+        GFXFramebufferDestroy(Object->cache.store);
+        Object->cache.store = NULL;
+    }
+    mtx_unlock(&Object->lock);
+}
+
 void GUIObjectAddChild(GUIObject Object, GUIObject Child)
 {
     CCAssertLog(Object, "GUI object must not be null");
@@ -151,8 +251,8 @@ void GUIObjectAddChild(GUIObject Object, GUIObject Child)
     mtx_lock(&Object->lock);
     Child->parent = Object;
     Object->interface->child.add(Object, Child);
-    Object->changed = TRUE;
-    Child->changed = TRUE;
+    Object->cache.changed = TRUE;
+    Child->cache.changed = TRUE;
     mtx_unlock(&Object->lock);
 }
 
@@ -165,8 +265,8 @@ void GUIObjectRemoveChild(GUIObject Object, GUIObject Child)
     mtx_lock(&Object->lock);
     Child->parent = NULL;
     Object->interface->child.remove(Object, Child);
-    Object->changed = TRUE;
-    Child->changed = TRUE;
+    Object->cache.changed = TRUE;
+    Child->cache.changed = TRUE;
     mtx_unlock(&Object->lock);
 }
 
@@ -175,10 +275,28 @@ _Bool GUIObjectHasChanged(GUIObject Object)
     CCAssertLog(Object, "GUI object must not be null");
     
     mtx_lock(&Object->lock);
-    const _Bool HasChanged = Object->changed ? TRUE : Object->interface->changed(Object);
+    _Bool Changed = Object->cache.changed;
+    if (!Changed)
+    {
+        if (Object->cache.strategy != GUIObjectCacheStrategyNone)
+        {
+            if (Object->cache.strategy & (GUIObjectCacheStrategyInvalidateOnMove | GUIObjectCacheStrategyInvalidateOnResize))
+            {
+                const CCRect Rect = GUIObjectGetRect(Object);
+                
+                Object->cache.strategy |= (((Object->cache.strategy & GUIObjectCacheStrategyInvalidateOnMove) && (CCVector2Equal(Rect.position, Object->cache.rect.position))) ? GUIObjectCacheDirtyMove : 0) | (((Object->cache.strategy & GUIObjectCacheStrategyInvalidateOnResize) && (CCVector2Equal(Rect.size, Object->cache.rect.size))) ? GUIObjectCacheDirtyResize : 0);
+                
+                Object->cache.rect = Rect;
+            }
+            
+            if (Object->cache.strategy & GUIObjectCacheDirtyMask) Changed = TRUE;
+        }
+        
+        if (!Changed) Changed = Object->interface->changed(Object);
+    }
     mtx_unlock(&Object->lock);
     
-    return HasChanged;
+    return Changed;
 }
 
 CCExpression GUIObjectEvaluateExpression(GUIObject Object, CCExpression Expression)
