ba197a0cf174639ee2ea8c70d2f78902f62d4bc8
Stefan Johnson <sj@scrimpycat.io>
2019-02-02 21:56:38 +1100

    Add header shader source type

--- src/rendering/utilities/pipeline/gfx/GFXShaderLibrary.h
+++ src/rendering/utilities/pipeline/gfx/GFXShaderLibrary.h
@@ -29,7 +29,7 @@
 #include <CommonC/Common.h>
 
 typedef enum {
-    //GFXShaderSourceTypeHeader, //TODO: add a header type
+    GFXShaderSourceTypeHeader,
     GFXShaderSourceTypeVertex,
     GFXShaderSourceTypeFragment
 } GFXShaderSourceType;
