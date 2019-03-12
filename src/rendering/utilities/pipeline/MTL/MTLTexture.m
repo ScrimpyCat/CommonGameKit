f309a590d3df67f3e84fe61dee43dac9f19754e8
Stefan Johnson <sj@scrimpycat.io>
2019-03-13 04:17:02 +1100

    Implement remaining texture functionality

--- src/rendering/utilities/pipeline/MTL/MTLTexture.m
+++ src/rendering/utilities/pipeline/MTL/MTLTexture.m
@@ -25,10 +25,44 @@
 
 #import "MTLTexture.h"
 #import "PixelDataStatic.h"
-#import <objc/objc.h>
-#import <objc/runtime.h>
+
+static MTLGFXTexture TextureConstructor(CCAllocatorType Allocator, GFXTextureHint Hint, CCColourFormat Format, size_t Width, size_t Height, size_t Depth, CCPixelData Data);
+static MTLGFXTexture TextureSubConstructor(CCAllocatorType Allocator, GFXTexture Root, size_t X, size_t Y, size_t Z, size_t Width, size_t Height, size_t Depth, CCPixelData Data);
+static void TextureDestructor(MTLGFXTexture Texture);
+static GFXTexture TextureGetParent(MTLGFXTexture Texture);
+static GFXTextureHint TextureGetHint(MTLGFXTexture Texture);
+static CCPixelData TextureGetData(MTLGFXTexture Texture);
+static void TextureGetOffset(MTLGFXTexture Texture, size_t *X, size_t *Y, size_t *Z);
+static void TextureGetInternalOffset(MTLGFXTexture Texture, size_t *X, size_t *Y, size_t *Z);
+static void TextureGetSize(MTLGFXTexture Texture, size_t *Width, size_t *Height, size_t *Depth);
+static void TextureGetInternalSize(MTLGFXTexture Texture, size_t *Width, size_t *Height, size_t *Depth);
+static void TextureSetFilterMode(MTLGFXTexture Texture, GFXTextureHint FilterType, GFXTextureHint FilterMode);
+static void TextureSetAddressMode(MTLGFXTexture Texture, GFXTextureHint Coordinate, GFXTextureHint AddressMode);
+static CCPixelData TextureRead(MTLGFXTexture Texture, CCAllocatorType Allocator, CCColourFormat Format, size_t X, size_t Y, size_t Z, size_t Width, size_t Height, size_t Depth);
+static void TextureWrite(MTLGFXTexture Texture, size_t X, size_t Y, size_t Z, size_t Width, size_t Height, size_t Depth, CCPixelData Data);
+static void *TextureGetStreamData(MTLGFXTexture Texture);
+static void TextureSetStreamData(MTLGFXTexture Texture, void *Stream);
+
 
 const GFXTextureInterface MTLTextureInterface = {
+    .create = (GFXTextureConstructorCallback)TextureConstructor,
+    .createSub = (GFXTextureSubConstructorCallback)TextureSubConstructor,
+    .destroy = (GFXTextureDestructorCallback)TextureDestructor,
+    .parent = (GFXTextureGetParentCallback)TextureGetParent,
+    .hints = (GFXTextureGetHintCallback)TextureGetHint,
+    .data = (GFXTextureGetDataCallback)TextureGetData,
+    .offset = (GFXTextureGetOffsetCallback)TextureGetOffset,
+    .size = (GFXTextureGetSizeCallback)TextureGetSize,
+    .setFilterMode = (GFXTextureSetFilterModeCallback)TextureSetFilterMode,
+    .setAddressMode = (GFXTextureSetAddressModeCallback)TextureSetAddressMode,
+    .read = (GFXTextureReadCallback)TextureRead,
+    .write = (GFXTextureWriteCallback)TextureWrite,
+    .getStream = (GFXTextureGetStreamDataCallback)TextureGetStreamData,
+    .setStream = (GFXTextureSetStreamDataCallback)TextureSetStreamData,
+    .optional = {
+        .internalOffset = (GFXTextureGetInternalOffsetCallback)TextureGetInternalOffset,
+        .internalSize = (GFXTextureGetInternalSizeCallback)TextureGetInternalSize
+    }
 };
 
 
@@ -168,7 +202,7 @@ static MTLGFXTexture TextureSubConstructor(CCAllocatorType Allocator, GFXTexture
             
             if (Pixels)
             {
-                [Root->root.texture replaceRegion: MTLRegionMake3D(0, 0, 0, Width, Height, Depth) mipmapLevel: 0 withBytes: Pixels bytesPerRow: CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0) * Width];
+                [Root->root.texture replaceRegion: MTLRegionMake3D(Texture->sub.internal.x, Texture->sub.internal.y, Texture->sub.internal.z, Width, Height, Depth) mipmapLevel: 0 withBytes: Pixels bytesPerRow: CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0) * Width];
                 
                 if (FreePixels) CCFree((void*)Pixels);
             }
@@ -181,4 +215,166 @@ static MTLGFXTexture TextureSubConstructor(CCAllocatorType Allocator, GFXTexture
 static void TextureDestructor(MTLGFXTexture Texture)
 {
     CC_SAFE_Free(Texture);
+}
+
+static GFXTexture TextureGetParent(MTLGFXTexture Texture)
+{
+    return Texture->isRoot ? NULL : Texture->sub.parent;
+}
+
+static GFXTextureHint TextureGetHint(MTLGFXTexture Texture)
+{
+    return Texture->isRoot ? Texture->root.hint : ((MTLGFXTexture)Texture->sub.internal.root)->root.hint;
+}
+
+static CCPixelData TextureGetData(MTLGFXTexture Texture)
+{
+    return Texture->data;
+}
+
+static void TextureGetOffset(MTLGFXTexture Texture, size_t *X, size_t *Y, size_t *Z)
+{
+    if (Texture->isRoot)
+    {
+        if (X) *X = 0;
+        if (Y) *Y = 0;
+        if (Z) *Z = 0;
+    }
+
+    else
+    {
+        if (X) *X = Texture->sub.x;
+        if (Y) *Y = Texture->sub.y;
+        if (Z) *Z = Texture->sub.z;
+    }
+}
+
+static void TextureGetInternalOffset(MTLGFXTexture Texture, size_t *X, size_t *Y, size_t *Z)
+{
+    if (Texture->isRoot)
+    {
+        if (X) *X = 0;
+        if (Y) *Y = 0;
+        if (Z) *Z = 0;
+    }
+
+    else
+    {
+        if (X) *X = Texture->sub.internal.x;
+        if (Y) *Y = Texture->sub.internal.y;
+        if (Z) *Z = Texture->sub.internal.z;
+    }
+}
+
+static void TextureGetSize(MTLGFXTexture Texture, size_t *Width, size_t *Height, size_t *Depth)
+{
+    if (Width) *Width = Texture->width;
+    if (Height) *Height = Texture->height;
+    if (Depth) *Depth = Texture->depth;
+}
+
+static void TextureGetInternalSize(MTLGFXTexture Texture, size_t *Width, size_t *Height, size_t *Depth)
+{
+    if (Texture->isRoot)
+    {
+        if (Width) *Width = Texture->width;
+        if (Height) *Height = Texture->height;
+        if (Depth) *Depth = Texture->depth;
+    }
+
+    else
+    {
+        if (Width) *Width = ((MTLGFXTexture)Texture->sub.internal.root)->width;
+        if (Height) *Height = ((MTLGFXTexture)Texture->sub.internal.root)->height;
+        if (Depth) *Depth = ((MTLGFXTexture)Texture->sub.internal.root)->depth;
+    }
+}
+
+static void TextureSetFilterMode(MTLGFXTexture Texture, GFXTextureHint FilterType, GFXTextureHint FilterMode)
+{
+    if (Texture->isRoot)
+    {
+        Texture->root.hint = (Texture->root.hint & ~(GFXTextureHintFilterModeMask << FilterType));
+    }
+
+    else CCAssertLog(0, "Cannot set a sub texture's filtering mode");
+}
+
+static void TextureSetAddressMode(MTLGFXTexture Texture, GFXTextureHint Coordinate, GFXTextureHint AddressMode)
+{
+    if (Texture->isRoot)
+    {
+        Texture->root.hint = (Texture->root.hint & ~(GFXTextureHintAddressModeMask << Coordinate));
+    }
+    
+    else CCAssertLog(0, "Cannot set a sub texture's filtering mode");
+}
+
+static CCPixelData TextureRead(MTLGFXTexture Texture, CCAllocatorType Allocator, CCColourFormat Format, size_t X, size_t Y, size_t Z, size_t Width, size_t Height, size_t Depth)
+{
+    MTLGFXTexture Root = Texture->isRoot ? Texture : (MTLGFXTexture)Texture->sub.internal.root;
+    
+    void *Pixels;
+    const size_t InternalSampleSize = CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0);
+    CC_SAFE_Malloc(Pixels, InternalSampleSize * Root->width * Root->height * Root->depth,
+                   CC_LOG_ERROR("Failed to allocate memory for texture receive. Allocation size (%zu)", InternalSampleSize * Width * Height * Depth);
+                   return NULL;
+                   );
+    
+    size_t RealX = 0, RealY = 0, RealZ = 0;
+    TextureGetInternalOffset(Texture, &RealX, &RealY, &RealZ);
+    
+    [Root->root.texture getBytes: Pixels bytesPerRow: CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0) * Width fromRegion: MTLRegionMake3D(RealX + X, RealY + Y, RealZ + Z, Width, Height, Depth) mipmapLevel: 0];
+    
+    CCPixelData CopiedData = CCPixelDataStaticCreate(CC_STD_ALLOCATOR, CCDataBufferCreate(CC_STD_ALLOCATOR, CCDataBufferHintFree, InternalSampleSize * Root->width * Root->height * Root->depth, Pixels, NULL, NULL), Root->root.format, Root->width, Root->height, Root->depth);
+    
+    const size_t SampleSize = CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0);
+    if (!(Pixels = CCMalloc(Allocator, SampleSize * Width * Height * Depth, NULL, CC_DEFAULT_ERROR_CALLBACK)))
+    {
+        CC_LOG_ERROR("Failed to allocate memory for texture receive. Allocation size (%zu)", SampleSize * Width * Height * Depth);
+        CCPixelDataDestroy(CopiedData);
+        return NULL;
+    }
+    
+    CCPixelDataGetPackedDataWithFormat(CopiedData, Format, 0, 0, 0, Width, Height, Depth, Pixels);
+    CCPixelDataDestroy(CopiedData);
+    
+    return CCPixelDataStaticCreate(Allocator, CCDataBufferCreate(Allocator, CCDataBufferHintFree, SampleSize * Width * Height * Depth, Pixels, NULL, NULL), Format, Width, Height, Depth);
+}
+
+static void TextureWrite(MTLGFXTexture Texture, size_t X, size_t Y, size_t Z, size_t Width, size_t Height, size_t Depth, CCPixelData Data)
+{
+    MTLGFXTexture Root = Texture->isRoot ? Texture : (MTLGFXTexture)Texture->sub.internal.root;
+    
+    _Bool FreePixels = FALSE;
+    const void *Pixels = (Data) && (Data->format == Root->root.format) && (CCPixelDataGetPlaneCount(Data) == 1) ? CCPixelDataGetBuffer(Data, CCColourFormatChannelPlanarIndex0) : NULL;
+    if ((!Pixels) && (Data))
+    {
+        FreePixels = TRUE;
+        
+        const size_t SampleSize = CCColourFormatSampleSizeForPlanar(Data->format, CCColourFormatChannelPlanarIndex0);
+        CC_SAFE_Malloc(Pixels, SampleSize * Width * Height * Depth,
+                       CC_LOG_ERROR("Failed to allocate memory for texture transfer. Allocation size (%zu)", SampleSize * Width * Height * Depth);
+                       return;
+                       );
+        
+        CCPixelDataGetPackedDataWithFormat(Data, Root->root.format, 0, 0, 0, Width, Height, Depth, (void*)Pixels);
+    }
+    
+    size_t RealX = 0, RealY = 0, RealZ = 0;
+    TextureGetInternalOffset(Texture, &RealX, &RealY, &RealZ);
+    
+    [Root->root.texture replaceRegion: MTLRegionMake3D(RealX + X, RealY + Y, RealZ + Z, Width, Height, Depth) mipmapLevel: 0 withBytes: Pixels bytesPerRow: CCColourFormatSampleSizeForPlanar(Root->root.format, CCColourFormatChannelPlanarIndex0) * Width];
+    
+    if (FreePixels) CCFree((void*)Pixels);
+}
+
+static void *TextureGetStreamData(MTLGFXTexture Texture)
+{
+    return Texture->stream;
+}
+
+static void TextureSetStreamData(MTLGFXTexture Texture, void *Stream)
+{
+    Texture->stream = Stream;
 }
