7eb2c830e2f528d44dce57d7f46e05dfb4687dea
Stefan Johnson <sj@scrimpycat.io>
2023-11-23 21:17:05 +1100

    Generate env defaults

--- gen_ecs_accessors.sh
+++ gen_ecs_accessors.sh
@@ -4,17 +4,5 @@ ecs_tool setup \
 --input src/ecs/systems/*.h src/ecs/components/*.h \
 --accessors src/ecs/systems/ECSSystemAccessors.h \
 --config ecs_env \
---write components:ids \
+--write components:ids components:env \
 $comp_ids_file
-
-echo '#ifndef ECS_COMPONENT_ID_BASE' >> $comp_ids_file
-echo '#define ECS_COMPONENT_ID_BASE 0' >> $comp_ids_file
-echo '#endif' >> $comp_ids_file
-echo '' >> $comp_ids_file
-echo '#ifndef ECS_MONITOR_COMPONENT_ID_RELATIVE' >> $comp_ids_file
-echo '#define ECS_MONITOR_COMPONENT_ID_RELATIVE 0' >> $comp_ids_file
-echo '#endif' >> $comp_ids_file
-echo '' >> $comp_ids_file
-echo '#ifndef ECS_MONITOR_COMPONENT_ID' >> $comp_ids_file
-echo '#define ECS_MONITOR_COMPONENT_ID (ECS_COMPONENT_ID_BASE + ECS_MONITOR_COMPONENT_ID_RELATIVE)' >> $comp_ids_file
-echo '#endif' >> $comp_ids_file
