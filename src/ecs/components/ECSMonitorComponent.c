db1a67299d28e844da62e1bcfc54dae77ee0c1bd
Stefan Johnson <sj@scrimpycat.io>
2023-12-17 06:22:05 +1100

    Enable quick compile

--- src/ecs/components/ECSMonitorComponent.c
+++ src/ecs/components/ECSMonitorComponent.c
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define CC_QUICK_COMPILE
 #include "ECSMonitorComponent.h"
 
 void ECSMonitorComponentDestructor(void *Monitor, ECSComponentID ID)
--- src/ecs/systems/ECSMonitorSystem.c
+++ src/ecs/systems/ECSMonitorSystem.c
@@ -23,6 +23,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define CC_QUICK_COMPILE
 #include "ECSMonitorSystem.h"
 #include "ECSSystemAccessors.h"
 #include "ECSComponentIDs.h"
